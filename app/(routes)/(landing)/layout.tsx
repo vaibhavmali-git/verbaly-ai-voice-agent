c37fd7223bd21acb823072f9ad086fa0e1e93410
vaibhavmali-git <malivaibhav.7620@gmail.com>
2026-05-22 09:51:51 +0530

    feat(frontend): implement header UI and configure app routing structure

--- app/(routes)/(landing)/layout.tsx
+++ app/(routes)/(landing)/layout.tsx
@@ -2,15 +2,15 @@ import Footer from "@/components/footer";
 import Header from "@/components/header";
 
 export default function LandingLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <div className="flex flex-col min-h-screen">
-            <Header />
-            <main className="flex-1">{children}</main>
-            <Footer /></div>
-
-    );
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex flex-1 flex-col items-center justify-center">{children}</main>
+      <Footer />
+    </div>
+  );
 }
--- app/(routes)/(landing)/page.tsx
+++ app/(routes)/(landing)/page.tsx
@@ -1,8 +1,35 @@
 import { Button } from "@/components/ui/custom/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <Button>Click</Button>
+    <div className="w-full max-w-5xl mx-auto flex flex-col md:flex-row items-center justify-center gap-2">
+      <div className="relative w-[380px] h-[380px] lg:w-[500px] lg:h-[500px] shrink-0">
+        <Image 
+        src={"/images/illustration.png"}
+        alt="Hero"
+        fill className="object-contain lg:mt-4"
+        priority/>
+      </div>
+
+      <div className="flex flex-col items-center gap-y-8 px-4">
+        <h1 className="text-center text-xl font-semibold text-[#4b4b4b] dark:text-white lg:text-3xl">The fun, effective, AI powered way to learn languages.</h1>
+     
+
+<div className="flex flex-col items-center gap-y-3 w-full max-w-[330px]">
+ {/* <Button size={"lg"} variant={"outline"} asChild>
+        <Link href={"/learn"}>Continue Learning</Link>
+      </Button> */}
+
+      <Button size={"lg"} asChild className="w-full">
+        <Link href={"/auth/sign-up"}>Get Started</Link>
+      </Button>
+      <Button size={"lg"} variant={"outline"} asChild className="w-full">
+        <Link href={"/auth/sign-in"}>I already have an account</Link>
+      </Button>
+      </div>
+    </div>
+     </div>
   );
 }
